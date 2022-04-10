dd6d83cc6b9847f7e78a537e2008b54087586c33
Carbon Bird Man <80457887+carbonbirdman@users.noreply.github.com>
2022-04-10 10:35:37 +1000

    merged filenames

--- scripts/merge_shorts.js
+++ scripts/merge_shorts.js
@@ -22,7 +22,7 @@ async function main() {
   console.log(merged);
 
   fs.writeFileSync(
-    "data/merged.json",
+    "data/merged_shorts.json",
     JSON.stringify(merged, undefined, 4),
     "utf8"
   );
