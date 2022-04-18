9ecc53ada10158103f7c9b0d144c778004874fc5
carbonbirdman <carbonbirdman@gmail.com>
2022-04-18 00:33:55 +0000

    more merging confusion

---
+++ scripts/cat_shortlists.js
@@ -0,0 +1,34 @@
+// merge into aggregated shortlist
+
+const fs = require("fs");
+//requiring path and fs modules
+const path = require("path");
+//joiing path of directory
+let directoryPath = path.join(__dirname, "data");
+directoryPath = "./data";
+
+async function main() {
+  let merged = [];
+  let files = fs.readdirSync(directoryPath);
+  let filteredFiles = files
+    .filter(
+      (fileName) =>
+        fileName.startsWith("merged_shortlist") & fileName.endsWith(".json")
+    )
+    .map((fileName) => {
+      merged = merged.concat(
+        JSON.parse(fs.readFileSync(directoryPath + "/" + fileName))
+      );
+      console.log(merged.length);
+    });
+
+  console.log(merged);
+
+  fs.writeFileSync(
+    "data/merged_shortlists_cat.json",
+    JSON.stringify(merged, undefined, 4),
+    "utf8"
+  );
+}
+
+main();
